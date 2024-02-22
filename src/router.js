3ac8fe0c1665f8ccbc8d8ea82edc13ad12b6dca8
Christian Alexander <christian.alexanderato@gmail.com>
2024-02-21 19:43:02 -0800

    dependencies added - pages/components - router

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import App from "./App";
+import Index from "./pages/index";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />}>
+
+        </Route>
+    )
+)
+
+export default router;
