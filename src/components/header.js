9c51c0f59c7120a8f91c46c7e1cf13f40059b90f
Christian Alexander <christian.alexanderato@gmail.com>
2024-03-04 19:57:09 -0800

    a friend helped me fix logging in - logout button added and working - app functions

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 function Header() {
+  const navigate = useNavigate();
+
     return (
       <div>
         You-do
+        <div>
+          <button onClick={() => {
+            localStorage.removeItem('token');
+            navigate("/")
+          }}>
+            Logout
+          </button>
+        </div>
       </div>
     )
   }
