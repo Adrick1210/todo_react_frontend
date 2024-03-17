b19f77dd2c9f40c4e49205516bb9a0a5b1f356c1
Christian Alexander <christian.alexanderato@gmail.com>
2024-03-16 21:12:08 -0700

    add modal works - edit modal in progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,9 @@ import { useContext } from "react";
 import { Form, Link } from "react-router-dom";
 import { TodoContext } from "../contexts/TodoContext";
 import { updateBoxToggle } from "../actions";
-import '../style.css'
+import { useState } from "react";
+import Modal from "../components/Modal";
+import "../style.css";
 
 function Index() {
   const { todos, updateTodo } = useContext(TodoContext);
@@ -15,15 +17,42 @@ function Index() {
     updateTodo(updatedTodo);
   };
 
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+
+  const handleOpenAddModal = () => {
+    setIsAddModalOpen(true);
+  };
+
+  const handleCloseAddModal = () => {
+    setIsAddModalOpen(false);
+  };
+
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+
+  const handleOpenEditModal = () => {
+    setIsEditModalOpen(true);
+  };
+
+  const handleCloseEditModal = () => {
+    setIsEditModalOpen(false);
+  };
+
   return (
     <div>
       <nav>
-        <Form action="/create" method="post">
-          <h2>Create a Todo</h2>
-          <input type="text" name="title" placeholder="Add Todo" />
-          <input type="checkbox" name="isComplete" />
-          <input type="submit" value={"Create"} />
-        </Form>
+        <button onClick={handleOpenAddModal}>Add</button>
+
+        {isAddModalOpen && (
+          <Modal>
+            <Form action="/create" method="post">
+              <h2>Create a Todo</h2>
+              <input type="text" name="title" placeholder="Add Todo" />
+              <input type="checkbox" name="isComplete" />
+              <input type="submit" value={"Create"} />
+              <button onClick={handleCloseAddModal}>Close</button>
+            </Form>
+          </Modal>
+        )}
       </nav>
 
       <div className="list-container">
@@ -41,6 +70,19 @@ function Index() {
                   onChange={() => handleCheckboxChange(todo)}
                 />
               </div>
+              {/* <button onClick={handleOpenEditModal}>Edit</button>
+
+              {isEditModalOpen && (
+                <Modal>
+                  <Form action="/create" method="post">
+                    <h2>Edit {todo.title}</h2>
+                    <input type="text" name="title" value={todo.title} />
+                    <input type="checkbox" name="isComplete" />
+                    <input type="submit" value={`Edit ${todo.title}`} />
+                    <button onClick={handleCloseEditModal}>Close</button>
+                  </Form>
+                </Modal>
+              )} */}
             </div>
           );
         })}
